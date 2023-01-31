caa1f7504f6452cebbecccae85f00587dc10a5b0
folt3k <folt3k@gmail.com>
2023-01-31 22:20:41 +0100

    done google sign in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,57 @@
 import {
   AfterViewInit,
+  ChangeDetectorRef,
   Component,
   ElementRef,
+  inject,
   OnInit,
   ViewChild,
 } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'oauth-test-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
+  imports: [CommonModule],
 })
 export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('googleSignInWrapper') googleSignInWrapperEl!: ElementRef;
 
-  title = 'oauth-test';
+  userInfo: { firstName: string; lastName: string; email: string } | null =
+    null;
+  private cdr = inject(ChangeDetectorRef);
 
   ngOnInit(): void {
+    this.initGoogleSignIn();
+  }
+
+  ngAfterViewInit(): void {
+    this.renderGoogleSignInBtn();
+  }
+
+  private renderGoogleSignInBtn(): void {
+    google.accounts.id.renderButton(this.googleSignInWrapperEl.nativeElement, {
+      type: 'standard',
+    });
+  }
+
+  private initGoogleSignIn(): void {
     google.accounts.id.initialize({
       client_id:
         '446066326969-j7f36bhn6uccqg1rq05bsqj8savusbic.apps.googleusercontent.com',
       callback: (response) => {
-        console.log(response);
+        const {
+          given_name: firstName,
+          family_name: lastName,
+          email,
+        } = jwtDecode<any>(response.credential);
+        this.userInfo = { firstName, lastName, email };
+        this.cdr.detectChanges();
       },
     });
-  }
-
-  ngAfterViewInit(): void {
-    google.accounts.id.renderButton(this.googleSignInWrapperEl.nativeElement, {
-      type: 'standard',
-    });
+    google.accounts.id.prompt();
   }
 }
